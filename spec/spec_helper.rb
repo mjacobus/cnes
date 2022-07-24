60caf48032919d7593bf3fe6b0a5cc9f34f015cd
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-07-24 20:10:27 -0300

    Add coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+require 'simplecov-lcov'
+require 'awesome_print'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::LcovFormatter
+  ]
+)
+
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.single_report_path = 'coverage/lcov.info'
+end
+
+if ENV['CI'] || ENV['COVERAGE']
+  SimpleCov.start do
+    add_filter '/spec'
+  end
+end
+
 require 'cnes'
 
 RSpec.configure do |config|
