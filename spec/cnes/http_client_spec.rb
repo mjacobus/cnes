bf11b64f18cc6f195b3f842865418752bea6e27d
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-07-24 21:44:50 -0300

    Cache requests with VCR

--- spec/cnes/http_client_spec.rb
+++ spec/cnes/http_client_spec.rb
@@ -6,53 +6,63 @@ RSpec.describe Cnes::HttpClient do
   let(:client) { Cnes::HttpClientFactory.new.create }
 
   it 'busca estabelecimentos' do
-    data = client.estabelecimentos
+    VCR.use_cassette('estabelecimentos') do
+      data = client.estabelecimentos
 
-    expect(data['estabelecimentos'].length).to eq(20)
+      expect(data['estabelecimentos'].length).to eq(20)
+    end
   end
 
   it 'busca estabelecimento by cnes' do
-    data = client.estabelecimento(16_993)
+    VCR.use_cassette('estabelecimentos/169993') do
+      data = client.estabelecimento(16_993)
 
-    expect(data['numero_cnpj_entidade']).to eq('76417005000186')
+      expect(data['numero_cnpj_entidade']).to eq('76417005000186')
+    end
   end
 
   it 'busca tipos de unidade' do
-    data = client.tipos_de_unidades
+    VCR.use_cassette('tipos_unidade') do
+      data = client.tipos_de_unidades
 
-    expect(data['tipos_unidade'].first['codigo_tipo_unidade']).to eq(80)
+      expect(data['tipos_unidade'].first['codigo_tipo_unidade']).to eq(80)
+    end
   end
 
   it 'busca tipo de unidade por código' do
-    data = client.tipo_de_unidade(80)
+    VCR.use_cassette('tipos_unidade/80') do
+      data = client.tipo_de_unidade(80)
 
-    expect(data['descricao_tipo_unidade']).to eq('LABORATORIO DE SAUDE PUBLICA')
+      expect(data['descricao_tipo_unidade']).to eq('LABORATORIO DE SAUDE PUBLICA')
+    end
   end
 
   describe '#profissionais' do
     it 'busca todos os profissionais por id do estabelecimento' do
-      data = client.profissionais('4314902237253')
+      VCR.use_cassette('profissionais/4314902237253') do
+        data = client.profissionais('4314902237253')
 
-      expect(data.first.keys).to eq(
-        %w[
-          tpSusNaoSus
-          cbo
-          dsCbo
-          chOutros
-          chAmb
-          chHosp
-          vinculacao
-          vinculo
-          subVinculo
-          nome
-          cns
-          cnsMaster
-          artigo2
-          artigo3
-          artigo5
-          dtEntrada
-        ]
-      )
+        expect(data.first.keys).to eq(
+          %w[
+            tpSusNaoSus
+            cbo
+            dsCbo
+            chOutros
+            chAmb
+            chHosp
+            vinculacao
+            vinculo
+            subVinculo
+            nome
+            cns
+            cnsMaster
+            artigo2
+            artigo3
+            artigo5
+            dtEntrada
+          ]
+        )
+      end
     end
   end
 end
